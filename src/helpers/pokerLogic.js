3e3daf0e4ca337d658fe86c79268c31f4ca8881d
Alex Vance <herdadvance@gmail.com>
2018-09-06 17:00:46 -0400

    Adding user ID to hand before comparing

--- src/helpers/pokerLogic.js
+++ src/helpers/pokerLogic.js
@@ -4,8 +4,11 @@ const pokerLogic = {
 
 	compare: function(board, hands){
 		var hand1 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Kc', 'Kd'])
-		var hand2 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Qs', 'Qd'])
+		hand1.user = "H1"
+		var hand2 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Ks', 'Kh'])
+		hand2.user = "H2"
 		var winner = Hand.winners([hand1, hand2])
+		console.log(winner)
 	}
 
 }
